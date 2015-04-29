d16ce3f2481f8e3e0747ebe2f8e44da0fd1c4ded
plagiares <plagiares@gmail.com>
2015-04-29 16:19:47 -0400

    Tutors and Students have been added to dbdsets

--- Sinapse/Sinapse/Repository/SinapseContext.cs
+++ Sinapse/Sinapse/Repository/SinapseContext.cs
@@ -23,36 +23,45 @@ namespace Sinapse.Repository
         protected override void Seed(SinapseContext appContext)
         {
             base.Seed(appContext);
+            Tutor tut1 = new Tutor() { ID = 1, LastName = "Bilodeau", FirstName = "Gary", EmailAddress = "bgary2@hotmail.com" };
+            Tutor tut2 = new Tutor() { ID = 2, LastName = "Gagnon", FirstName = "Samuel", EmailAddress = "samPP92@hotmail.com" };
+            Tutor tut3 = new Tutor() { ID = 3, LastName = "Gingras", FirstName = "Simon", EmailAddress = "ptitguy22@bell.net" };
+            Tutor tut4 = new Tutor() { ID = 4, LastName = "Roy", FirstName = "Éric", EmailAddress = "eroy231@videotron.ca" };
+            Tutor tut5 = new Tutor() { ID = 5, LastName = "Veilleux", FirstName = "Caroline", EmailAddress = "caro.koko@hotmail.com" };
+            Tutor tut6 = new Tutor() { ID = 6, LastName = "Tremblay", FirstName = "Karine", EmailAddress = "kar.tremblay@gmail.com" };
+            Tutor tut7 = new Tutor() { ID = 7, LastName = "Vézina", FirstName = "Louis", EmailAddress = "vl410Bd@gmail.com" };
 
-            Employe emp1 = new Employe() { EmployeID = 100, NAS = 123456789, Nom = "Bond", Prenom = "James", Salaire = 120000 };
-            Employe emp2 = new Employe() { EmployeID = 101, NAS = 234567890, Nom = "Frost", Prenom = "Miranda", Salaire = 110000 };
-            Employe emp3 = new Employe() { EmployeID = 102, NAS = 345678901, Nom = "Lynd", Prenom = "Vesper", Salaire = 99000 };
-            Employe emp4 = new Employe() { EmployeID = 103, NAS = 456789012, Nom = "King", Prenom = "Elektra", Salaire = 1500000 };
-            Employe emp5 = new Employe() { EmployeID = 104, NAS = 567890123, Nom = "Jones", Prenom = "Christmas", Salaire = 88000 };
+            appContext.Tutors.Add(tut1);
+            appContext.Tutors.Add(tut2);
+            appContext.Tutors.Add(tut3);
+            appContext.Tutors.Add(tut4);
+            appContext.Tutors.Add(tut5);
+            appContext.Tutors.Add(tut6);
+            appContext.Tutors.Add(tut7);
 
-            Client cl1 = new Client() { ClientID = 10, NomClient = "Secret Client 1" };
-            Client cl2 = new Client() { ClientID = 11, NomClient = "Secret Client 2" };
-            Projet p1 = new Projet() { ProjetID = 10, Nom = "BrightDiamond", ClientID = cl1 };
-            Projet p2 = new Projet() { ProjetID = 11, Nom = "InvisibleArctica", ClientID = cl1 };
-            Projet p3 = new Projet() { ProjetID = 12, Nom = "GoldenEye", ClientID = cl2 };
-            Projet p4 = new Projet() { ProjetID = 13, Nom = "TomorrowNeverDies", ClientID = cl2 };
-            Projet p5 = new Projet() { ProjetID = 13, Nom = "TheWorldIsNotEnough", ClientID = cl2 };
+            HelpedStudent stu1 = new HelpedStudent(){ID = 100, LastName = "Arsenault", FirstName = "Marc", EmailAddress = "marco.arso@hotmail.com"}
+			HelpedStudent stu2 = new HelpedStudent(){ID = 101, LastName = "Boilard", FirstName = "Eric", EmailAddress = "eric.r.boilard2@coop.com"}
+			HelpedStudent stu3 = new HelpedStudent(){ID = 102, LastName = "Couture", FirstName = "Jean-Cristophe", EmailAddress = "jc.couture.wilde@hotmail.com"}
+			HelpedStudent stu4 = new HelpedStudent(){ID = 103, LastName = "Desrosiers", FirstName = "Julianne", EmailAddress = "galypo13@hotmail.com"}
+			HelpedStudent stu5 = new HelpedStudent(){ID = 104, LastName = "Grégoire-Allen", FirstName = "Léo", EmailAddress = "leoga@hotmail.com"}
+			HelpedStudent stu6 = new HelpedStudent(){ID = 105, LastName = "Hamel", FirstName = "François", EmailAddress = "fran.ham@hotmail.com"}
+			HelpedStudent stu7 = new HelpedStudent(){ID = 106, LastName = "Lepage", FirstName = "Jérémy", EmailAddress = "je.lepa@hotmail.com"}
+			HelpedStudent stu8 = new HelpedStudent(){ID = 107, LastName = "Poitras", FirstName = "Nicolas", EmailAddress = "nic.poi@hotmail.com"}
+			HelpedStudent stu9 = new HelpedStudent(){ID = 108, LastName = "Roy-Gagnon", FirstName = "Samuel", EmailAddress = "sam.roy@hotmail.com"}
+			HelpedStudent stu10 = new HelpedStudent(){ID = 109, LastName = "Simard", FirstName = "Benjamin", EmailAddress = "ben.simard@hotmail.com"}
+			HelpedStudent stu11 = new HelpedStudent(){ID = 110, LastName = "Vachon", FirstName = "Samuel", EmailAddress = "sam.vachon@hotmail.com"}
 
-            appContext.Employes.Add(emp1);
-            appContext.Employes.Add(emp2);
-            appContext.Employes.Add(emp3);
-            appContext.Employes.Add(emp4);
-            appContext.Employes.Add(emp5);
-            appContext.Employes.Add(new Employe() { EmployeID = 105, NAS = 678901234, Nom = "Jonhson", Prenom = "Jinx", Salaire = 100000 });
-            appContext.Employes.Add(new Employe() { EmployeID = 106, NAS = 789012345, Nom = "Onatopp", Prenom = "Xenia", Salaire = 1900000 });
-
-            appContext.Clients.Add(cl1);
-            appContext.Clients.Add(cl2);
-            appContext.TutoringSessions.Add(p1);
-            appContext.TutoringSessions.Add(p2);
-            appContext.TutoringSessions.Add(p3);
-            appContext.TutoringSessions.Add(p4);
-            appContext.TutoringSessions.Add(p5);
+            appContext.HelpedStudents.Add(stu1);
+			appContext.HelpedStudents.Add(stu2);
+			appContext.HelpedStudents.Add(stu3);
+			appContext.HelpedStudents.Add(stu4);
+			appContext.HelpedStudents.Add(stu5);
+			appContext.HelpedStudents.Add(stu6);
+			appContext.HelpedStudents.Add(stu7);
+			appContext.HelpedStudents.Add(stu8);
+			appContext.HelpedStudents.Add(stu9);
+			appContext.HelpedStudents.Add(stu10);
+			appContext.HelpedStudents.Add(stu11);
 
             appContext.Affectations.Add(new Affectation { AffectationID = 1001, DateAffectation = DateTime.Today, EmployeID = emp1, ProjetID = p1 });
             appContext.Affectations.Add(new Affectation { AffectationID = 1002, DateAffectation = DateTime.Today, EmployeID = emp1, ProjetID = p2 });
