a8cafbf296a18424f10b6d6823785ad57bcbfe39
plagiares <plagiares@gmail.com>
2015-04-29 15:58:23 -0400

    TutoringSession has been created

---
+++ Sinapse/Sinapse/Model/TutoringSession.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinapse.Model
+{
+    class TutoringSession
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public virtual Tutor TutorID { get; set; }
+
+        [Required]
+        public virtual HelpedStudent HelpedID { get; set; }
+
+        [Required]
+        public DateTime DateSession { get; set; }
+
+        [Required]
+        public DateTime TimeSession { get; set; }
+
+        [Required]
+        public int LengthSession { get; set; }
+    }
+}
