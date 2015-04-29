1c5999785572702b807ddfeb45aa2ea4b7f0bf2b
plagiares <plagiares@gmail.com>
2015-04-29 15:48:03 -0400

    HelpedStudent and Tutor Entities created

---
+++ Sinapse/Sinapse/Model/HelpedStudent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sinapse.Model
+{
+    public class HelpedStudent
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string EmailAddress { get; set; }
+
+        public virtual ICollection<TutoringSession> TutoringSessions { get; set; }
+    }
+}
