b5610193b5be6bb57119aefc3df69626a76c5ba1
Maxime <maxnard35@gmail.com>
2015-05-04 21:34:27 -0400

    Adds not forget, add 9 last final remise 1

--- Sinapse/Sinapse/Repository/SinapseContext.cs
+++ Sinapse/Sinapse/Repository/SinapseContext.cs
@@ -63,7 +63,7 @@ namespace Sinapse.Repository
 			appContext.HelpedStudents.Add(stu10);
 			appContext.HelpedStudents.Add(stu11);
 
-            TutoringSession tgs1 = new TutoringSession() { ID = 100, DateSession = new DateTime(2015,03,16), TimeSession = 13, LengthSession = 2, HelpedID = stu5, TutorID = tut5};
+            TutoringSession tgs1 = new TutoringSession() { ID = 100, DateSession = new DateTime(2015, 03, 16), TimeSession = 13, LengthSession = 2, HelpedID = stu5, TutorID = tut5};
             TutoringSession tgs2 = new TutoringSession() { ID = 101, DateSession = new DateTime(2015, 03, 24), TimeSession = 10, LengthSession = 1, HelpedID = stu5, TutorID = tut1 };
             TutoringSession tgs3 = new TutoringSession() { ID = 102, DateSession = new DateTime(2015, 03, 25), TimeSession = 13, LengthSession = 1, HelpedID = stu2, TutorID = tut4 };
             TutoringSession tgs4 = new TutoringSession() { ID = 103, DateSession = new DateTime(2015, 04, 01), TimeSession = 12, LengthSession = 2, HelpedID = stu1, TutorID = tut5 };
@@ -72,6 +72,34 @@ namespace Sinapse.Repository
             TutoringSession tgs7 = new TutoringSession() { ID = 106, DateSession = new DateTime(2015, 04, 08), TimeSession = 10, LengthSession = 1, HelpedID = stu6, TutorID = tut4 };
             TutoringSession tgs8 = new TutoringSession() { ID = 107, DateSession = new DateTime(2015, 04, 10), TimeSession = 10, LengthSession = 2, HelpedID = stu1, TutorID = tut4 };
             TutoringSession tgs9 = new TutoringSession() { ID = 108, DateSession = new DateTime(2015, 04, 29), TimeSession = 12, LengthSession = 1, HelpedID = stu9, TutorID = tut5 };
+            TutoringSession tgs10 = new TutoringSession() { ID = 109, DateSession = new DateTime(2015, 05, 25), TimeSession = 13, LengthSession = 1, HelpedID = stu10, TutorID = tut7 };
+            TutoringSession tgs11 = new TutoringSession() { ID = 110, DateSession = new DateTime(2015, 05, 27), TimeSession = 12, LengthSession = 2, HelpedID = stu10, TutorID = tut5 };
+            TutoringSession tgs12 = new TutoringSession() { ID = 111, DateSession = new DateTime(2015, 05, 27), TimeSession = 13, LengthSession = 1, HelpedID = stu2, TutorID = tut3 };
+            TutoringSession tgs13 = new TutoringSession() { ID = 112, DateSession = new DateTime(2015, 06, 01), TimeSession = 9, LengthSession = 1, HelpedID = stu7, TutorID = tut7 };
+            TutoringSession tgs14 = new TutoringSession() { ID = 113, DateSession = new DateTime(2015, 06, 02), TimeSession = 9, LengthSession = 1, HelpedID = stu11, TutorID = tut7 };
+            TutoringSession tgs15 = new TutoringSession() { ID = 114, DateSession = new DateTime(2015, 06, 02), TimeSession = 11, LengthSession = 1, HelpedID = stu2, TutorID = tut7 };
+            TutoringSession tgs16 = new TutoringSession() { ID = 115, DateSession = new DateTime(2015, 06, 02), TimeSession = 13, LengthSession = 1, HelpedID = stu5, TutorID = tut1 };
+            TutoringSession tgs17 = new TutoringSession() { ID = 116, DateSession = new DateTime(2015, 06, 02), TimeSession = 15, LengthSession = 1, HelpedID = stu9, TutorID = tut4 };
+            TutoringSession tgs18 = new TutoringSession() { ID = 117, DateSession = new DateTime(2015, 06, 03), TimeSession = 13, LengthSession = 2, HelpedID = stu7, TutorID = tut3 };
+
+            appContext.TutoringSessions.Add(tgs1);
+            appContext.TutoringSessions.Add(tgs2);
+            appContext.TutoringSessions.Add(tgs3);
+            appContext.TutoringSessions.Add(tgs4);
+            appContext.TutoringSessions.Add(tgs5);
+            appContext.TutoringSessions.Add(tgs6);
+            appContext.TutoringSessions.Add(tgs7);
+            appContext.TutoringSessions.Add(tgs8);
+            appContext.TutoringSessions.Add(tgs9);
+            appContext.TutoringSessions.Add(tgs10);
+            appContext.TutoringSessions.Add(tgs11);
+            appContext.TutoringSessions.Add(tgs12);
+            appContext.TutoringSessions.Add(tgs13);
+            appContext.TutoringSessions.Add(tgs14);
+            appContext.TutoringSessions.Add(tgs15);
+            appContext.TutoringSessions.Add(tgs16);
+            appContext.TutoringSessions.Add(tgs17);
+            appContext.TutoringSessions.Add(tgs18);
 
         }
     }
